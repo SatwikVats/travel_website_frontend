6f6448022481ca3a914e699cf2f10ff3b8c50c56
SatwikVats <f20190194@pilani.bits-pilani.ac.in>
2023-06-18 21:10:44 +0530

    Added Nav-search bar.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,6 +6,14 @@ export const Navbar = () => {
             <h1 className="heading-title">
                 <a className="link" href="/">CheckInn</a>
             </h1>
+            <div className="form-container d-flex align-center cursor-pointer shadow" >
+                <span className="form-option">Any Where</span>
+                <span className="border-right-1px"></span>
+                <span className="form-option">Any Week</span>
+                <span className="border-right-1px"></span>
+                <span className="form-option">Add Guests</span>
+                <span className="search material-icons-outlined">search</span>
+            </div>
             <nav className="d-flex align-centre gap-large">
                 <div className="nav d-flex align-centre cursor-pointer">
                     <span className="material-icons-outlined profile-option menu">menu</span>
