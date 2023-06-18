9fcb21154cf7b52f773757c59085a0c510743495
SatwikVats <f20190194@pilani.bits-pilani.ac.in>
2023-06-19 00:17:51 +0530

    Minor changes.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,9 +15,9 @@ export const Navbar = () => {
                 <span className="search material-icons-outlined">search</span>
             </div>
             <nav className="d-flex align-centre gap-large">
-                <div className="nav d-flex align-centre cursor-pointer">
-                    <span className="material-icons-outlined profile-option menu">menu</span>
-                    <span className="material-icons-outlined profile-option person">account_circle</span>
+                <div className="nav d-flex align-center cursor-pointer">
+                    <span className="material-icons profile-option menu">menu</span>
+                    <span className="material-icons profile-option person">account_circle</span>
                 </div>
             </nav>
         </header>
