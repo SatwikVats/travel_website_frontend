4f1b215d802e211b4e6775116768aba5229d9997
SatwikVats <f20190194@pilani.bits-pilani.ac.in>
2023-06-19 14:06:08 +0530

    Created hook to display hotel data.

--- src/components/HotelCard/HotelCard.jsx
+++ src/components/HotelCard/HotelCard.jsx
@@ -1,27 +1,30 @@
 import "./HotelCard.css";
 
-export const HotelCard = () => {
+export const HotelCard = ({hotel}) => {
+
+    const{ _id, name, image, address, state, rating, price } = hotel;
+
     return(
         <div className="relative hotelcard-container shadow cursor-pointer">
             <div>
-                <img className="img" src="https://a0.muscache.com/im/pictures/ae654a2a-a0a4-42cd-8d1f-f8cc97c005cc.jpg?im_w=720" alt="hotelcard" />
+                <img className="img" src={image} alt={name} />
                 <div className="hotelcard-details">
                     <div className="d-flex align-center">
-                        <span className="location">Bir, Himachal Pradesh</span>
+                        <span className="location">{address}, {state}</span>
                         <span className="rating d-flex align-center">
                             <span class="material-icons-outlined">star</span>
-                            <span>4.3</span>
+                            <span>{rating}</span>
                         </span>
                     </div>                  
-                    <p className="hotel-name">Sukoon Baag</p>
+                    <p className="hotel-name">{name}</p>
                     <p className="price details">
-                        <span className="price">Rs. 3500</span>
+                        <span className="price">Rs. {price}</span>
                         <span className="night">night</span>
                     </p>
                 </div>               
             </div>            
             <button className="button btn-wishlist absolute">
-                <span class="material-icons favorite cursor">favorite</span>
+                <span className="material-icons favorite cursor">favorite</span>
             </button>            
         </div>
     )
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,39 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 import { Navbar, HotelCard } from "../../components";
 import "./Home.css";
 
 export const Home = () => {
+
+    const [hasMore, setHasMore] = useState(true);
+    const [hotelsToShow, setHotelsToShow] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(16);
+
+    const [hotels, setHotels] = useState([]);
+    
+    useEffect(()=>{
+        (async () => {
+            try{
+                const {data} = await axios.get("https://dull-blue-reindeer-vest.cyclic.app/api/hotels");
+                setHotels(data? data.slice(0,16) : []);    //To display only first 16 entries.
+            }
+            catch(err){
+                console.log(err);
+            }
+        })();
+    },[]);
+
+    //const fetchMoreData
+
     return(
         <Fragment>
             <Navbar />
-            <main className="main">
-                <HotelCard />
+            <main className="main d-flex align-center wrap gap-larger">
+                {
+                    hotels && hotels.map(hotel => <HotelCard key={hotel._id} hotel={hotel}/>)
+                }
             </main>
 
         </Fragment>
