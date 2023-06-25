09738fc429b73fcb9e7c831f41d2ef51f06097ad
SatwikVats <f20190194@pilani.bits-pilani.ac.in>
2023-06-25 23:40:57 +0530

    Added Signup validation using Regex.

--- src/components/Auth/AuthSignup.jsx
+++ src/components/Auth/AuthSignup.jsx
@@ -1,42 +1,83 @@
 import "./Auth.css";
 import { useAuth } from "../../context";
+import { validateName, validateEmail, validateNumber, validatePassword } from "../../utils";
 
 export const AuthSignup = () => {
     const {name, email, password, number, confirmPassword, authDispatch} = useAuth();
 
+    console.log({name, email, password, number, confirmPassword});
+
     const handleNumberChange = (event) => {
-        authDispatch({
-            type: "NUMBER",
-            payload: event.target.value
-        })
+        const isNumberValid = validateNumber(event.target.value);
+        if(isNumberValid){
+            console.log("Valid number");
+            authDispatch({
+                type: "NUMBER",
+                payload: event.target.value
+            })
+        }
+        else{
+            console.log("Invalid number");
+        }
     }
+
 
     const handleEmailChange = (event) => {
-        authDispatch({
-            type: "EMAIL",
-            payload: event.target.value
-        })
+        const isEmailValid = validateEmail(event.target.value);
+        if(isEmailValid){
+            console.log("Valid email");
+            authDispatch({
+                type: "EMAIL",
+                payload: event.target.value
+            })
+        }
+        else{
+            console.log("Invalid email");
+        }
     }
+
 
     const handleNameChange = (event) => {
-        authDispatch({
-            type: "NAME",
-            payload: event.target.value
-        })
+        const isNameValid = validateName(event.target.value);
+        if(isNameValid){
+            console.log("Valid name");
+            authDispatch({
+                type: "NAME",
+                payload: event.target.value
+            })
+        }
+        else{
+            console.log("Invalid name");
+        }
     }
 
+
     const handlePasswordChange = (event) => {
-        authDispatch({
-            type: "PASSWORD",
-            payload: event.target.value
-        })
+        const isPasswordValid = validatePassword(event.target.value);
+        if(isPasswordValid){
+            console.log("Valid password");
+            authDispatch({
+                type: "PASSWORD",
+                payload: event.target.value
+            })
+        }
+        else{
+            console.log("Invalid password");
+        }
     }
 
     const handleConfirmPasswordChange = (event) => {
-        authDispatch({
-            type: "CONFIRM_PASSWORD",
-            payload: event.target.value
-        })
+        const isConfirmPasswordValid = validatePassword(event.target.value);
+        if(isConfirmPasswordValid){
+            console.log("Valid confirmPassword.");
+            authDispatch({
+                type: "CONFIRM_PASSWORD",
+                payload: event.target.value
+            })
+        }
+        else{
+            console.log("Invalid confirmPassword.");
+        }
     }
 
     return(
@@ -44,27 +85,27 @@ export const AuthSignup = () => {
             <form>
                 <div className="d-flex direction-column lb-in-container">
                     <label className="auth-label" type="number">Mobile Number <span className="asterisk">*</span>{" "}</label>
-                    <input className="auth-input" placeholder="Enter Mobile Number" maxLength="10" value={number}
+                    <input className="auth-input" placeholder="Enter Mobile Number" maxLength="10" defaultValue={number}
                     onChange={handleNumberChange} required/>
                 </div>
                 <div className="d-flex direction-column lb-in-container">
                     <label className="auth-label">Name <span className="asterisk">*</span>{" "}</label>
-                    <input className="auth-input" placeholder="Enter Name" value={name}
+                    <input className="auth-input" placeholder="Enter Name" defaultValue={name}
                     onChange={handleNameChange} required/>
                 </div>
                 <div className="d-flex direction-column lb-in-container">
                     <label className="auth-label">Email <span className="asterisk">*</span>{" "}</label>
-                    <input className="auth-input" placeholder="Enter Email" type="email" value={email} 
+                    <input className="auth-input" placeholder="Enter Email" type="email" defaultValue={email} 
                     onChange={handleEmailChange} required/>
                 </div>
                 <div className="d-flex direction-column lb-in-container">
                     <label className="auth-label">Password <span className="asterisk">*</span>{" "}</label>
-                    <input className="auth-input" placeholder="Enter Password" type="password" value={password} 
+                    <input className="auth-input" placeholder="Enter Password" type="password" defaultValue={password} 
                     onChange={handlePasswordChange} required/>
                 </div>
                 <div className="d-flex direction-column lb-in-container">
                     <label className="auth-label">Confirm Password <span className="asterisk">*</span>{" "}</label>
-                    <input className="auth-input" placeholder="Enter Password" type="password" value={confirmPassword}
+                    <input className="auth-input" placeholder="Enter Password" type="password" defaultValue={confirmPassword}
                     onChange={handleConfirmPasswordChange} required/>
                 </div>
                 <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,7 @@ export {GetHotelsByRoomsAndBeds} from "./room-beds";
 export {GetHotelsByPropertyType} from "./property";
 export {GetHotelsByRating} from "./rating";
 export {GetHotelsByCancellation} from "./hotel-cancel";
+export {validateName} from "./name-regex";
+export {validateEmail} from "./email-regex";
+export {validatePassword} from "./password-regex";
+export {validateNumber} from "./number-regex";
